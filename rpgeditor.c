9aa67896960d8c5aa3b32e7447fc7239fad79384
Graph3x <hejzlar.filip@seznam.cz>
2024-03-20 20:10:54 +0100

    cache bufferand window size

--- rpgeditor.c
+++ rpgeditor.c
@@ -5,6 +5,8 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <errno.h>
+#include <sys/ioctl.h>
+#include <string.h>
 
 /*** defines ***/
 #define CTRL_KEY(k) ((k) & 0x1f)
@@ -14,16 +16,76 @@
 struct editorConfig
 {
     struct termios original_term_mode;
+    int screen_rows;
+    int screen_cols;
 };
 
-struct editorConfig EditConf;
+struct editorConfig edit_conf;
+
+struct cache_buffer
+{
+    char *cbuffer;
+    int len;
+};
+#define CBUFFER_INIT \
+    {                \
+        NULL, 0      \
+    }
 
 /*** functions ***/
 
+void cBAppend(struct cache_buffer *cb, const char *s, int len)
+{
+    char *new = realloc(cb->cbuffer, cb->len + len);
+    if (new == NULL)
+        return;
+    memcpy(&new[cb->len], s, len);
+    cb->cbuffer = new;
+    cb->len += len;
+}
+void cBFree(struct cache_buffer *cb)
+{
+    free(cb->cbuffer);
+}
+
+int getWindowSize(int *rows, int *cols)
+{
+    struct winsize ws;
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0)
+    {
+        return -1;
+    }
+    else
+    {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+void editorDrawRows(struct cache_buffer *cbuf)
+{
+    for (int y = 0; y < edit_conf.screen_rows; y++)
+    {
+        cBAppend(cbuf, "~", 1);
+
+        if (y < edit_conf.screen_rows - 1)
+        {
+            cBAppend(cbuf, "\r\n", 2);
+        }
+    }
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 void editorRefreshScreen()
 {
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
+    struct cache_buffer cb = CBUFFER_INIT;
+    cBAppend(&cb, "\x1b[2J", 4);
+    cBAppend(&cb, "\x1b[H", 3);
+    editorDrawRows(&cb);
+    cBAppend(&cb, "\x1b[H", 3);
+    write(STDOUT_FILENO, cb.cbuffer, cb.len);
+    cBFree(&cb);
 }
 
 void die(const char *s)
@@ -35,7 +97,7 @@ void die(const char *s)
 
 void disableRawMode()
 {
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &EditConf.original_term_mode) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &edit_conf.original_term_mode) == -1)
     {
         die("tcsetattr");
     }
@@ -43,11 +105,11 @@ void disableRawMode()
 
 void enableRawMode()
 {
-    if (tcgetattr(STDIN_FILENO, &EditConf.original_term_mode) == -1)
+    if (tcgetattr(STDIN_FILENO, &edit_conf.original_term_mode) == -1)
     {
         die("tcgetattr");
     }
-    struct termios raw = EditConf.original_term_mode;
+    struct termios raw = edit_conf.original_term_mode;
 
     raw.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
     raw.c_oflag &= ~(OPOST);
@@ -59,15 +121,6 @@ void enableRawMode()
     }
 
     atexit(disableRawMode);
-}
-
-void editorDrawRows()
-{
-    for (int y = 0; y < 24; y++)
-    {
-        write(STDOUT_FILENO, "~\r\n", 3);
-    }
-    write(STDOUT_FILENO, "\x1b[H", 3);
 }
 
 char editorReadKey()
@@ -113,9 +166,9 @@ void editorProcessKeypress()
 int main()
 {
     enableRawMode();
+    if (getWindowSize(&edit_conf.screen_rows, &edit_conf.screen_cols) == -1)
+        die("getWindowSize");
     editorRefreshScreen();
-    // draw "graphics"
-    editorDrawRows();
 
     while (1)
     {
